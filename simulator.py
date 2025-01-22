277a2ba8b32acf7734529fa259d343b526833400
MathioLucas <162763121+MathioLucas@users.noreply.github.com>
2025-01-21 22:52:25 -0500

    Update simulator.py
    created Expedition team class

--- simulator.py
+++ simulator.py
@@ -1,1 +1,17 @@
 
+import random
+import time
+
+class Expedition:
+    def __init__(self, era, location, budget):
+        self.era = era
+        self.location = location
+        self.budget = budget
+        self.team = []
+        self.equipment = []
+        self.supplies = 100  # Represents percentage
+        self.time_remaining = 100  # Represents days
+        self.artifacts = []
+        self.reputation = 0
+
+   
